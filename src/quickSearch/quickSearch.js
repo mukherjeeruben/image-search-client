465c4e0710c9f5d7cd32074380d8ad92037f3c4c
mukherjeeruben <rubenmukherjee@outlook.com>
2022-04-07 11:41:33 +0100

    Update quickSearch.js
    style changes for search bar

--- src/quickSearch/quickSearch.js
+++ src/quickSearch/quickSearch.js
@@ -4,13 +4,43 @@ import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 import SearchIcon from "@material-ui/icons/Search";
 import DisplayImages from "./images/display-images";
+import { red } from "@material-ui/core/colors";
+import { alpha } from "@material-ui/core/styles/colorManipulator";
+
+const Pointer = {cursor: 'pointer'};
+const TextBoxStyle = {height: '50px',
+                      width: "600px",
+                      display: 'flex',
+                      alignSelf: 'initial'};
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexDirection: "column",
     alignItems: 'center',
-    margin: '3% auto'
+    margin: '10% auto',
+    '& .MuiFilledInput-adornedEnd': {
+      backgroundColor: "#fcfcfb",
+      border: "1px solid #e2e2e1",
+      overflow: "hidden",
+      borderRadius: 4,
+      transition: theme.transitions.create(["border-color", "box-shadow"]),
+      "&:hover": {
+        backgroundColor: "#fff",
+        text: 'Search'
+      },
+      "&$focused": {
+        backgroundColor: "#fff",
+        boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
+        borderColor: theme.palette.primary.main
+      }
+    },
+    '& .MuiFilledInput-input':{
+      padding: '11px 12px 10px'
+    },
+    '& .MuiSvgIcon-root':{
+      padding: '1px 2px 14px'
+    }
   },
   margin: {
     margin: theme.spacing(1),
@@ -18,15 +48,15 @@ const useStyles = makeStyles((theme) => ({
   withoutLabel: {
     marginTop: theme.spacing(3),
   },
-  textField: {
-    width: "70ch",
-    padding: "12px 12px 12px",
-    "&>div:first-child>div:last-child": {
-      display: 'flex',
-      marginTop: '0',
-      alignSelf: 'initial'
-    }
-  }
+  // textField: {
+  //   width: "70ch",
+  //   padding: "12px 12px 12px",
+  //   "&>div:first-child>div:last-child": {
+  //     display: 'flex',
+  //     marginTop: '0',
+  //     alignSelf: 'initial'
+  //   }
+  // }
 }));
 
 const QuickSearch = (props) => {
@@ -48,17 +78,19 @@ const QuickSearch = (props) => {
   return (
     <div className={classes.root}>
       <FormControl
-        className={clsx(classes.margin, classes.textField)}
+        className={clsx(classes.margin)}
         variant="filled"
       >
         <FilledInput
           id="filled-adornment-weight"
-          placeholder="Search Text"
+          style = {TextBoxStyle}
+          // placeholder="Search Text"
           value={search}
           onChange={(e) => setSearchText(e.target.value)}
           endAdornment={
             <InputAdornment position="start">
-              <SearchIcon onClick={searchTextData} />
+              <SearchIcon onClick={searchTextData} 
+                          style={Pointer} />
             </InputAdornment>
           }
           aria-describedby="filled-weight-helper-text"
